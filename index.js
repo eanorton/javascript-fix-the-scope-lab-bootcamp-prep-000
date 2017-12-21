d1dedf80cb3ec56598a2d18d88162f0830057753
Edward Norton <edwardnorton@icloud.com>
2017-12-21 21:49:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var animal = 'dog'
+var animal = 'cat'
 
 function myAnimal() {
   return animal
@@ -12,7 +12,7 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + n
+  return n + two
 
   // Feel free to move things around!
   const two = 2
